e4a09dcf4f6185acee36b1fb7be935352b4752bc
Landen <lipskochlanden@gmail.com>
2026-03-02 11:25:58 -0600

    version 1.0.1

--- main.tf
+++ main.tf
@@ -60,6 +60,25 @@ resource "docker_container" "kali_desktop" {
   }
 }
 
+#DVWA Challenge Container
+#######################################################################################################################
+resource "docker_image" "dvwa" {
+  name         = "vulnerables/web-dvwa:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "dvwa" {
+  name  = "dvwa-challenge"
+  image = docker_image.dvwa.image_id
+  memory = 256
+
+  networks_advanced {
+    name = docker_network.lab_network.name
+  }
+
+  # No external ports - only accessible from Kali via internal network
+}
+
 
 # Image/container template (internal network)
 #######################################################################################################################
