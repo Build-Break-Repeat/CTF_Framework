87ef8f68e41971044c4d9a348a4625d2c0e7d9ac
Landen <lipskochlanden@gmail.com>
2026-03-04 20:07:23 -0600

    team 1 container flag creation

---
+++ scripts/createflags.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+#usage:
+#chmod +x scripts/createflags.py
+#python3 scripts/createflags.py
+
+import os
+import secrets
+
+challenges = [
+    "web_dvwa",
+    "sqli_labs",
+    "juice_shop",
+    "webgoat",
+    "bwapp",
+    "nowasp",
+    "metasploitable2"
+]
+
+#create flag folder
+os.makedirs("flags", exist_ok=True)
+
+##############################################################################################
+team = 1
+
+print("\ncreating flags\n")
+
+for challenge in challenges:
+    flag = f"flag:{{{challenge}_team{team}}}"
+    
+    #write flags to flag folder
+    with open(f"flags/{challenge}_team{team}.txt", "w") as f:
+        f.write(flag)
+
+    print(f"{flag}")
+
+print("\nflags created and saved to flags folder\n")
+
+##############################################################################################
