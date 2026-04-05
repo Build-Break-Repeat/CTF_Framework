d75702bf9f58bd9abd1bb70f7d60ce8077ec83c4
Logan Anderson <ABL72003@gmail.com>
2026-04-05 16:21:05 -0500

    added challenge JSON file for all the contents to become more dynamic. UIBehavior.js is almost done. placeholder function in there and updated challenges.html

---
+++ registration-site/js/UIbehavior.js
@@ -0,0 +1,58 @@
+//This will load the challenges when the page challenges page loads.
+document.addEventListener("DOMContentLoaded", () => {
+    if (document.getElementById("challenge-container")) {
+        //this wil be called to load the challenges
+        loadChallenges();
+    }
+});
+
+
+//this function fetches the challenges from the JSON file.
+async function loadChallenges() {
+    try {
+        //this is the location of the information
+        const response = await fetch("../challenges.json");
+        const challenges =await response.json();
+
+        const container = document.getElementById("challenge-container");
+
+        container.innerHTML = "";
+
+        challenges.forEach(challenge => {
+            const card = createChallengesCard(challenge);
+            container.appendChild(card);
+        });
+    }
+
+    catch (error) {
+        console.error("Error loading challenges.", error);
+    }
+}
+
+
+//This will create the card for the challenges.
+
+function createChallengesCard(challenge) {
+    const div = document.createElement("div");
+    div.className = "challenge-card";
+
+    div.innerHTML = `
+    <h3>${challenge.name}</h3>
+    <p>${challenge.description}</p>
+    <span class="tag">${challenge.type}</span>
+    <button onclick= "startChallenge('${challenge.name}')">
+        Start Challenge
+    </button>
+    
+    
+     `;
+
+    return div;
+}
+
+
+//placeholder function it will be replaced for when we actually need to call backend
+function startChallenge(name) {
+    console.log("Starting challenge:", name);
+    alert(`Starting ${name}`);
+}
