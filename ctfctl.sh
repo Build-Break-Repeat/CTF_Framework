03d602899c2cef63ed307df22b809cf7b79f38dc
Zack Lewis <97800571+zack-lewis@users.noreply.github.com>
2026-04-15 13:28:19 -0500

    Fixing location of go src file

--- ctfctl.sh
+++ ctfctl.sh
@@ -2,8 +2,9 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$SCRIPT_DIR"
+CTFCTL_DIR="$SCRIPT_DIR/src/ctfctl"
+cd "$CTFCTL_DIR" || exit 1
+
 exec go run ctfctl.go "$@"
 
-
-#this is just a wrapper script that allows ctfctl.go to function
+# this is just a wrapper script that allows ctfctl.go to function without compiling
